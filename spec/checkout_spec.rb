3cf972f4e71b3ff85b68fa96fd4952833d9dfff6
elrok123 <conner@systemicconfluence.com>
2018-07-24 08:31:25 +0100

    Renaming RSpec files to get rspec to recursively look for tests

---
+++ spec/checkout_spec.rb
@@ -0,0 +1,26 @@
+require "./lib/Checkout"
+
+describe "Checkout" do
+    describe ".new"
+        context "when we create a new instance with rules" do
+            it "should return a checkout instance" do
+                # Create some rules to be used
+                rules = {
+                    items: [
+                            Item.new(name: "A", price: 30, discount_quantity: 3, discount_amount: 15),
+                            Item.new(name: "B", price: 20, discount_quantity: 2, discount_amount: 5)
+                            Item.new(name: "C", price: 50, discount_quantity: 0, discount_amount: 0)
+                            Item.new(name: "D", price: 15, discount_quantity: 0, discount_amount: 0)
+                    ]    
+                }
+                assert(Checkout.new(rules) != nil)
+            end
+        end
+    end
+    describe "#scan"
+        context "when given a new item" do
+            it "should update the total price"
+            end
+        end
+    end
+end
---
+++ spec/item_spec.rb
@@ -0,0 +1,13 @@
+require "./lib/Item"
+
+describe Item do
+#    describe ".new"
+#        context "when given valid item parameters"
+#            it "should return a new item"
+#                item = Item.new()
+#                assert(item != nil)
+#            end
+#        end
+#    end
+end
+

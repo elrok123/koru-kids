6fee46461f20c5f0306af726b60366bb37a5e811
elrok123 <conner@systemicconfluence.com>
2018-07-24 08:44:15 +0100

    Adding New Methods to Checkout class
    - Added base method "total" for returning a total checkout price
    - Added "update_total" method
    - Added "scan" method for adding a new item to the checkout and updating the price

--- lib/Checkout.rb
+++ lib/Checkout.rb
@@ -1,3 +1,28 @@
 # Class for running a checkout process
 class Checkout
+    @item_rules = []
+    @current_items = []
+    @total_price = 0
+
+    # Sets up the base class instance
+    def initialize(rules)
+       @item_rules = rules 
+
+    end
+
+    # Updates the total after adding a new item
+    def update_total
+
+    end
+    
+    # Scans an item into the system
+    def scan(item)
+        @current_items << item    
+        self.update_total
+    end
+    
+    # Returns the checkout total minus item discounts
+    def total
+       return @total_price 
+    end
 end
